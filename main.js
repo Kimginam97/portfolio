45fc864a3f760721573bc3f27921cb5f97cc24a5
Kimginam97 <65409092+Kimginam97@users.noreply.github.com>
2020-07-04 12:45:51 +0900

    JS:Scroll Navbar

--- main.js
+++ main.js
@@ -7,8 +7,22 @@ document.addEventListener('scroll',()=>{
     console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY>navbarHeight){
         navbar.classList.add('navbar--dark');
-    }
-    else{
+    } else {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu=document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event)=>{
+    const target=event.target;
+    const link=target.dataset.link;
+    if(link==null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo=document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth' });
+    
+});
+
